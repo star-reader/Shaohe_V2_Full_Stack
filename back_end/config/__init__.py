ad8a0b3f69bb57684cbba2874e263ed8dc7e9a41
star-reader <75555433+star-reader@users.noreply.github.com>
2025-03-18 14:50:24 +0800

    Update __init__.py

--- back_end/config/__init__.py
+++ back_end/config/__init__.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://shaohe:8h3wdd5YYmzzzWaj@154.37.214.155/shaohe'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'yd32gfycpg923cy3gwycgewy'
-    JWT_SECRET_KEY = 'yd32gfycpg923cy3gwycgewy'
+    SECRET_KEY = ''
+    JWT_SECRET_KEY = ''
